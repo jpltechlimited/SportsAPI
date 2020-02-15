112b737ee44c01d5216de1b3df56dc948d922708
joselozano <jose@lozano>
2020-02-15 14:16:18 +0000

    no message

--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 from sportsapi import views
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
+    url(r'^', include_docs_urls(title='Sports API')),
     url(r'^api/auth', include('rest_framework.urls', namespace='rest_framework')),
     path(r'admin/', admin.site.urls),
     path(r'api/tennisplayers/<str:pk>/', views.TennisPlayerAPIView.as_view())
