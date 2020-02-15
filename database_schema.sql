ba5d7cecf7163e17af15ecfc97b62762571c1e7e
joselozano <jose@lozano>
2020-02-15 09:37:08 +0000

    no message

---
+++ database_schema.sql
@@ -0,0 +1,420 @@
+/****** Object:  Database [SportApiDB]    Script Date: 15/02/2020 09:36:43 ******/
+CREATE DATABASE [SportApiDB]  (EDITION = 'Basic', SERVICE_OBJECTIVE = 'Basic', MAXSIZE = 2 GB) WITH CATALOG_COLLATION = SQL_Latin1_General_CP1_CI_AS;
+GO
+ALTER DATABASE [SportApiDB] SET ANSI_NULL_DEFAULT OFF 
+GO
+ALTER DATABASE [SportApiDB] SET ANSI_NULLS OFF 
+GO
+ALTER DATABASE [SportApiDB] SET ANSI_PADDING OFF 
+GO
+ALTER DATABASE [SportApiDB] SET ANSI_WARNINGS OFF 
+GO
+ALTER DATABASE [SportApiDB] SET ARITHABORT OFF 
+GO
+ALTER DATABASE [SportApiDB] SET AUTO_SHRINK OFF 
+GO
+ALTER DATABASE [SportApiDB] SET AUTO_UPDATE_STATISTICS ON 
+GO
+ALTER DATABASE [SportApiDB] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+ALTER DATABASE [SportApiDB] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+ALTER DATABASE [SportApiDB] SET NUMERIC_ROUNDABORT OFF 
+GO
+ALTER DATABASE [SportApiDB] SET QUOTED_IDENTIFIER OFF 
+GO
+ALTER DATABASE [SportApiDB] SET RECURSIVE_TRIGGERS OFF 
+GO
+ALTER DATABASE [SportApiDB] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+ALTER DATABASE [SportApiDB] SET ALLOW_SNAPSHOT_ISOLATION ON 
+GO
+ALTER DATABASE [SportApiDB] SET PARAMETERIZATION SIMPLE 
+GO
+ALTER DATABASE [SportApiDB] SET READ_COMMITTED_SNAPSHOT ON 
+GO
+ALTER DATABASE [SportApiDB] SET  MULTI_USER 
+GO
+ALTER DATABASE [SportApiDB] SET ENCRYPTION ON
+GO
+ALTER DATABASE [SportApiDB] SET QUERY_STORE = ON
+GO
+ALTER DATABASE [SportApiDB] SET QUERY_STORE (OPERATION_MODE = READ_WRITE, CLEANUP_POLICY = (STALE_QUERY_THRESHOLD_DAYS = 7), DATA_FLUSH_INTERVAL_SECONDS = 900, INTERVAL_LENGTH_MINUTES = 60, MAX_STORAGE_SIZE_MB = 10, QUERY_CAPTURE_MODE = AUTO, SIZE_BASED_CLEANUP_MODE = AUTO)
+GO
+/****** Object:  Table [dbo].[auth_group]    Script Date: 15/02/2020 09:36:43 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[auth_group](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[name] [nvarchar](80) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY],
+UNIQUE NONCLUSTERED 
+(
+	[name] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[auth_group_permissions]    Script Date: 15/02/2020 09:36:43 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[auth_group_permissions](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[group_id] [int] NOT NULL,
+	[permission_id] [int] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY],
+ CONSTRAINT [auth_group_permissions_group_id_permission_id_0cd325b0_uniq] UNIQUE NONCLUSTERED 
+(
+	[group_id] ASC,
+	[permission_id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[auth_permission]    Script Date: 15/02/2020 09:36:43 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[auth_permission](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[name] [nvarchar](255) NOT NULL,
+	[content_type_id] [int] NOT NULL,
+	[codename] [nvarchar](100) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY],
+ CONSTRAINT [auth_permission_content_type_id_codename_01ab375a_uniq] UNIQUE NONCLUSTERED 
+(
+	[content_type_id] ASC,
+	[codename] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[auth_user]    Script Date: 15/02/2020 09:36:43 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[auth_user](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[password] [nvarchar](128) NOT NULL,
+	[last_login] [datetime2](7) NULL,
+	[is_superuser] [bit] NOT NULL,
+	[username] [nvarchar](150) NOT NULL,
+	[first_name] [nvarchar](30) NOT NULL,
+	[last_name] [nvarchar](150) NOT NULL,
+	[email] [nvarchar](254) NOT NULL,
+	[is_staff] [bit] NOT NULL,
+	[is_active] [bit] NOT NULL,
+	[date_joined] [datetime2](7) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY],
+ CONSTRAINT [auth_user_username_6821ab7c_uniq] UNIQUE NONCLUSTERED 
+(
+	[username] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[auth_user_groups]    Script Date: 15/02/2020 09:36:43 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[auth_user_groups](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[user_id] [int] NOT NULL,
+	[group_id] [int] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY],
+ CONSTRAINT [auth_user_groups_user_id_group_id_94350c0c_uniq] UNIQUE NONCLUSTERED 
+(
+	[user_id] ASC,
+	[group_id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[auth_user_user_permissions]    Script Date: 15/02/2020 09:36:43 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[auth_user_user_permissions](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[user_id] [int] NOT NULL,
+	[permission_id] [int] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY],
+ CONSTRAINT [auth_user_user_permissions_user_id_permission_id_14a6b632_uniq] UNIQUE NONCLUSTERED 
+(
+	[user_id] ASC,
+	[permission_id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[django_admin_log]    Script Date: 15/02/2020 09:36:43 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[django_admin_log](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[action_time] [datetime2](7) NOT NULL,
+	[object_id] [nvarchar](max) NULL,
+	[object_repr] [nvarchar](200) NOT NULL,
+	[action_flag] [smallint] NOT NULL,
+	[change_message] [nvarchar](max) NOT NULL,
+	[content_type_id] [int] NULL,
+	[user_id] [int] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[django_content_type]    Script Date: 15/02/2020 09:36:43 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[django_content_type](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[app_label] [nvarchar](100) NOT NULL,
+	[model] [nvarchar](100) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY],
+ CONSTRAINT [django_content_type_app_label_model_76bd3d3b_uniq] UNIQUE NONCLUSTERED 
+(
+	[app_label] ASC,
+	[model] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[django_migrations]    Script Date: 15/02/2020 09:36:43 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[django_migrations](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[app] [nvarchar](255) NOT NULL,
+	[name] [nvarchar](255) NOT NULL,
+	[applied] [datetime2](7) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[django_session]    Script Date: 15/02/2020 09:36:43 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[django_session](
+	[session_key] [nvarchar](40) NOT NULL,
+	[session_data] [nvarchar](max) NOT NULL,
+	[expire_date] [datetime2](7) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[session_key] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[tennis_match]    Script Date: 15/02/2020 09:36:43 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[tennis_match](
+	[id] [nvarchar](50) NOT NULL,
+	[day] [datetime2](7) NULL,
+	[player1_id] [nvarchar](50) NOT NULL,
+	[player2_id] [nvarchar](50) NOT NULL,
+	[event] [nvarchar](50) NULL,
+	[location] [nvarchar](50) NULL,
+	[court_type] [nvarchar](50) NULL,
+	[tournament_stage] [nvarchar](50) NULL,
+ CONSTRAINT [pk_tennis_match] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[tennis_match_point_by_point]    Script Date: 15/02/2020 09:36:43 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[tennis_match_point_by_point](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[match_id] [nvarchar](max) NOT NULL,
+	[stage] [nvarchar](50) NULL,
+	[player_a_games] [int] NOT NULL,
+	[player_b_games] [int] NOT NULL,
+	[player_a_point] [nvarchar](2) NULL,
+	[player_b_point] [nvarchar](2) NULL,
+	[break_point] [bit] NOT NULL,
+	[set_point] [bit] NOT NULL,
+	[match_point] [bit] NOT NULL,
+	[player_a_serving] [bit] NOT NULL,
+	[player_b_serving] [bit] NOT NULL,
+ CONSTRAINT [PK_tennis_match_point_by_point] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[tennis_player]    Script Date: 15/02/2020 09:36:43 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[tennis_player](
+	[id] [nvarchar](50) NOT NULL,
+	[fullname] [nvarchar](50) NULL,
+	[url] [nvarchar](100) NULL,
+ CONSTRAINT [PK_TennisPlayer] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Index [auth_group_permissions_group_id_b120cbf9]    Script Date: 15/02/2020 09:36:43 ******/
+CREATE NONCLUSTERED INDEX [auth_group_permissions_group_id_b120cbf9] ON [dbo].[auth_group_permissions]
+(
+	[group_id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, DROP_EXISTING = OFF, ONLINE = OFF) ON [PRIMARY]
+GO
+/****** Object:  Index [auth_group_permissions_permission_id_84c5c92e]    Script Date: 15/02/2020 09:36:43 ******/
+CREATE NONCLUSTERED INDEX [auth_group_permissions_permission_id_84c5c92e] ON [dbo].[auth_group_permissions]
+(
+	[permission_id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, DROP_EXISTING = OFF, ONLINE = OFF) ON [PRIMARY]
+GO
+/****** Object:  Index [auth_permission_content_type_id_2f476e4b]    Script Date: 15/02/2020 09:36:43 ******/
+CREATE NONCLUSTERED INDEX [auth_permission_content_type_id_2f476e4b] ON [dbo].[auth_permission]
+(
+	[content_type_id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, DROP_EXISTING = OFF, ONLINE = OFF) ON [PRIMARY]
+GO
+/****** Object:  Index [auth_user_groups_group_id_97559544]    Script Date: 15/02/2020 09:36:43 ******/
+CREATE NONCLUSTERED INDEX [auth_user_groups_group_id_97559544] ON [dbo].[auth_user_groups]
+(
+	[group_id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, DROP_EXISTING = OFF, ONLINE = OFF) ON [PRIMARY]
+GO
+/****** Object:  Index [auth_user_groups_user_id_6a12ed8b]    Script Date: 15/02/2020 09:36:43 ******/
+CREATE NONCLUSTERED INDEX [auth_user_groups_user_id_6a12ed8b] ON [dbo].[auth_user_groups]
+(
+	[user_id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, DROP_EXISTING = OFF, ONLINE = OFF) ON [PRIMARY]
+GO
+/****** Object:  Index [auth_user_user_permissions_permission_id_1fbb5f2c]    Script Date: 15/02/2020 09:36:43 ******/
+CREATE NONCLUSTERED INDEX [auth_user_user_permissions_permission_id_1fbb5f2c] ON [dbo].[auth_user_user_permissions]
+(
+	[permission_id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, DROP_EXISTING = OFF, ONLINE = OFF) ON [PRIMARY]
+GO
+/****** Object:  Index [auth_user_user_permissions_user_id_a95ead1b]    Script Date: 15/02/2020 09:36:43 ******/
+CREATE NONCLUSTERED INDEX [auth_user_user_permissions_user_id_a95ead1b] ON [dbo].[auth_user_user_permissions]
+(
+	[user_id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, DROP_EXISTING = OFF, ONLINE = OFF) ON [PRIMARY]
+GO
+/****** Object:  Index [django_admin_log_content_type_id_c4bce8eb]    Script Date: 15/02/2020 09:36:43 ******/
+CREATE NONCLUSTERED INDEX [django_admin_log_content_type_id_c4bce8eb] ON [dbo].[django_admin_log]
+(
+	[content_type_id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, DROP_EXISTING = OFF, ONLINE = OFF) ON [PRIMARY]
+GO
+/****** Object:  Index [django_admin_log_user_id_c564eba6]    Script Date: 15/02/2020 09:36:43 ******/
+CREATE NONCLUSTERED INDEX [django_admin_log_user_id_c564eba6] ON [dbo].[django_admin_log]
+(
+	[user_id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, DROP_EXISTING = OFF, ONLINE = OFF) ON [PRIMARY]
+GO
+/****** Object:  Index [django_session_expire_date_a5c62663]    Script Date: 15/02/2020 09:36:43 ******/
+CREATE NONCLUSTERED INDEX [django_session_expire_date_a5c62663] ON [dbo].[django_session]
+(
+	[expire_date] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, DROP_EXISTING = OFF, ONLINE = OFF) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[auth_group_permissions]  WITH CHECK ADD  CONSTRAINT [auth_group_permissions_group_id_b120cbf9_fk_auth_group_id] FOREIGN KEY([group_id])
+REFERENCES [dbo].[auth_group] ([id])
+GO
+ALTER TABLE [dbo].[auth_group_permissions] CHECK CONSTRAINT [auth_group_permissions_group_id_b120cbf9_fk_auth_group_id]
+GO
+ALTER TABLE [dbo].[auth_group_permissions]  WITH CHECK ADD  CONSTRAINT [auth_group_permissions_permission_id_84c5c92e_fk_auth_permission_id] FOREIGN KEY([permission_id])
+REFERENCES [dbo].[auth_permission] ([id])
+GO
+ALTER TABLE [dbo].[auth_group_permissions] CHECK CONSTRAINT [auth_group_permissions_permission_id_84c5c92e_fk_auth_permission_id]
+GO
+ALTER TABLE [dbo].[auth_permission]  WITH CHECK ADD  CONSTRAINT [auth_permission_content_type_id_2f476e4b_fk_django_content_type_id] FOREIGN KEY([content_type_id])
+REFERENCES [dbo].[django_content_type] ([id])
+GO
+ALTER TABLE [dbo].[auth_permission] CHECK CONSTRAINT [auth_permission_content_type_id_2f476e4b_fk_django_content_type_id]
+GO
+ALTER TABLE [dbo].[auth_user_groups]  WITH CHECK ADD  CONSTRAINT [auth_user_groups_group_id_97559544_fk_auth_group_id] FOREIGN KEY([group_id])
+REFERENCES [dbo].[auth_group] ([id])
+GO
+ALTER TABLE [dbo].[auth_user_groups] CHECK CONSTRAINT [auth_user_groups_group_id_97559544_fk_auth_group_id]
+GO
+ALTER TABLE [dbo].[auth_user_groups]  WITH CHECK ADD  CONSTRAINT [auth_user_groups_user_id_6a12ed8b_fk_auth_user_id] FOREIGN KEY([user_id])
+REFERENCES [dbo].[auth_user] ([id])
+GO
+ALTER TABLE [dbo].[auth_user_groups] CHECK CONSTRAINT [auth_user_groups_user_id_6a12ed8b_fk_auth_user_id]
+GO
+ALTER TABLE [dbo].[auth_user_user_permissions]  WITH CHECK ADD  CONSTRAINT [auth_user_user_permissions_permission_id_1fbb5f2c_fk_auth_permission_id] FOREIGN KEY([permission_id])
+REFERENCES [dbo].[auth_permission] ([id])
+GO
+ALTER TABLE [dbo].[auth_user_user_permissions] CHECK CONSTRAINT [auth_user_user_permissions_permission_id_1fbb5f2c_fk_auth_permission_id]
+GO
+ALTER TABLE [dbo].[auth_user_user_permissions]  WITH CHECK ADD  CONSTRAINT [auth_user_user_permissions_user_id_a95ead1b_fk_auth_user_id] FOREIGN KEY([user_id])
+REFERENCES [dbo].[auth_user] ([id])
+GO
+ALTER TABLE [dbo].[auth_user_user_permissions] CHECK CONSTRAINT [auth_user_user_permissions_user_id_a95ead1b_fk_auth_user_id]
+GO
+ALTER TABLE [dbo].[django_admin_log]  WITH CHECK ADD  CONSTRAINT [django_admin_log_content_type_id_c4bce8eb_fk_django_content_type_id] FOREIGN KEY([content_type_id])
+REFERENCES [dbo].[django_content_type] ([id])
+GO
+ALTER TABLE [dbo].[django_admin_log] CHECK CONSTRAINT [django_admin_log_content_type_id_c4bce8eb_fk_django_content_type_id]
+GO
+ALTER TABLE [dbo].[django_admin_log]  WITH CHECK ADD  CONSTRAINT [django_admin_log_user_id_c564eba6_fk_auth_user_id] FOREIGN KEY([user_id])
+REFERENCES [dbo].[auth_user] ([id])
+GO
+ALTER TABLE [dbo].[django_admin_log] CHECK CONSTRAINT [django_admin_log_user_id_c564eba6_fk_auth_user_id]
+GO
+ALTER TABLE [dbo].[tennis_match]  WITH CHECK ADD  CONSTRAINT [FK_tennis_match_tennis_player_player1_id] FOREIGN KEY([player1_id])
+REFERENCES [dbo].[tennis_player] ([id])
+GO
+ALTER TABLE [dbo].[tennis_match] CHECK CONSTRAINT [FK_tennis_match_tennis_player_player1_id]
+GO
+ALTER TABLE [dbo].[tennis_match]  WITH CHECK ADD  CONSTRAINT [FK_tennis_match_tennis_player_player2_id] FOREIGN KEY([player2_id])
+REFERENCES [dbo].[tennis_player] ([id])
+GO
+ALTER TABLE [dbo].[tennis_match] CHECK CONSTRAINT [FK_tennis_match_tennis_player_player2_id]
+GO
+ALTER TABLE [dbo].[django_admin_log]  WITH CHECK ADD  CONSTRAINT [django_admin_log_action_flag_a8637d59_check] CHECK  (([action_flag]>=(0)))
+GO
+ALTER TABLE [dbo].[django_admin_log] CHECK CONSTRAINT [django_admin_log_action_flag_a8637d59_check]
+GO
+ALTER DATABASE [SportApiDB] SET  READ_WRITE 
+GO
